2cca5ca768ace93998ad3b1106ae15ea3ba28621
Svetlana Pervoikina <svetlanapervoikina@mail.ru>
2022-07-06 23:28:30 +0400

    ДЗ3 черновик

---
+++ ДЗ3.cs
@@ -0,0 +1,14 @@
+int []array = new array [5]
+{
+    {
+        if array [0] = array [4];
+        if array [1] = array [3];
+
+    Console.WriteLine("Данное число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Данное число-не палиндром")
+    }
+}
+
