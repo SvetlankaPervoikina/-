6508b2d273b8b584e0158203977a8940e4b454c6
Svetlana Pervoikina <svetlanapervoikina@mail.ru>
2022-07-06 23:49:23 +0400

    условия ДЗ2

---
+++ ДЗ2.cs
@@ -0,0 +1,50 @@
+/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int SecondNum
+{
+    int num=new random().Next(100;1000);
+    Console.WriteLine("Текущее трехзначное число +"num")
+
+    int m=num/10;
+    int n=m%10;
+}
+return n
+*/
+
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+int IndexOf(int[]collection, int find);
+int count = collection.Length;
+int index = 0;
+int position = 0;
+While (index>=count)
+{
+    position=index;
+}
+return position
+*/
+
+/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int i 
+{
+    {
+        while (i<5);
+        Console.WriteLine("Указанный день - будний");
+    }
+    else
+    {
+        if 5<i<7
+        Console.WriteLine("Указанный день - выходной");
+    }
+    else 
+    {
+        ConsoleWriteline("Такого дня не существует!")
+    }
+}
+
+
+
+
+
