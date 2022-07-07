002b7b79057adc687851fc42b018d3578d16a43a
Svetlanka <svetlanapervoikina@mail.ru>
2022-07-08 00:01:33 +0400

    Дз2 проверить

--- ДЗ2.cs
+++ ДЗ2.cs
@@ -1,50 +1,51 @@
-/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int SecondNum
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*void SecondNumVoid (int number, int m, int n)
 {
-    int num=new random().Next(100;1000);
-    Console.WriteLine("Текущее трехзначное число +"num")
-
-    int m=num/10;
-    int n=m%10;
+    int number = new Random().Next(100;1000);
+    Console.WriteLine("Текущее число " + number);
+    int m = number / 10;
+    int n = m % 10;
 }
-return n
+ return n;
+
+SecondNumVoid()
 */
 
-/*
-Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
-
-int IndexOf(int[]collection, int find);
-int count = collection.Length;
-int index = 0;
-int position = 0;
-While (index>=count)
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int IndexOf(int[]collection, int find)
 {
-    position=index;
+    int count = 3;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection [index] == find)
+        {
+            position = index;
+        }
+    }
+        index ++;
+    return position;
 }
-return position
+
+Console.WriteLine("Input number;");
+int num = Convert.ToInt32(Console.ReadLine());
+
+IndexOf(num);
+
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/*void WeekendOrNot (int numday)
+{
+    if (numday >= 1 && numday <= 7);
+    {
+        if (numday >= 1 && numday <= 5) Console.WriteLine ("This day is weekday");
+        if (numday >= 6 && numday <= 7) Console.WriteLine("This day is weekend");
+    }
+}
+Console.WriteLine("Input a number day");
+int numday = Convert.ToInt32(Console.ReadLine());
+
+WeekendOrNot(numday);
 */
-
-/*
-Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
-int i 
-{
-    {
-        while (i<5);
-        Console.WriteLine("Указанный день - будний");
-    }
-    else
-    {
-        if 5<i<7
-        Console.WriteLine("Указанный день - выходной");
-    }
-    else 
-    {
-        ConsoleWriteline("Такого дня не существует!")
-    }
-}
-
-
-
-
-
