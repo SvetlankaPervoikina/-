395d56dd5cd1a1d3b70204606cd0d1602b966008
«Svetlana <svetlanapervoikina@mail.ru>
2022-07-08 13:19:37 +0300

    первое решение второй задачи

---
+++ ДЗ4.cs
@@ -0,0 +1,32 @@
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+/*int [] RandomMassive (int m, int minValue, int maxValue)
+{
+    int [] newMassive = new int [m];
+    for (int i = 0; int i< m; i++);
+    
+    newMassive [i] = new Random().Nex(minValue, maxValue+1)
+return newMassive (int[]array)
+}
+void newMassive (int[]array)
+{
+    for (int i=0; i<array.Length; i++) Console.Write("Array[i]+ "  );
+
+Console.WriteLine();
+}
+    
+Console.WriteLine("Введите размер массива:  ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальный предел массива:  ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальный предел массива:  ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int RandomMassive(m);
+*/
+
+
+
