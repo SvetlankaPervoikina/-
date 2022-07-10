a606e969779952882935dfe06024126889deb3c4
Svetlanka <svetlanapervoikina@mail.ru>
2022-07-11 00:19:54 +0400

    проверить

--- ДЗ4.cs
+++ ДЗ4.cs
@@ -1,9 +1,26 @@
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+int SummaChisel(int i)
+{
+    int result = 0;
+    while (i > 0)
+        {
+            int num = i % 10;
+            i = i / 10;
+            result = result + num;
+        }
+        return result;
+}
+Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
 
-
+int result = SummaChisel(num); 
+Console.WriteLine($"Сумма цифр в числе {num} равна {result}");
+*/
 
 // Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
-int [] RandomMassive (int m, int minValue, int maxValue)
+/*
+int[]RandomMassive (int m, int minValue, int maxValue)
 {
     int [] newMassive = new int [m];
     for (int i = 0; int i < m; i++);
@@ -26,7 +43,7 @@ Console.WriteLine("Введите максимальный предел масс
 int maxValue = Convert.ToInt32(Console.ReadLine());
 
 int RandomMassive(m);
-
+*/
 
 
 
