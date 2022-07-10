65a2002da1d8473edc757a9d5d1dc7a1172a74a0
Svetlanka <svetlanapervoikina@mail.ru>
2022-07-10 22:25:37 +0400

    исправила ввод данных

--- ДЗ3.cs
+++ ДЗ3.cs
@@ -28,20 +28,20 @@ int number = Convert.ToInt32(console.ReadLine());
 
 
 // Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-/*
-int () tablicaKubov = int (int i, int N, int m, int j)
+
+int tablicaKubov(int i, int N, int m, int j)
 {
-    for (int i=1, i <=N, i++);
+    for (int i=1; i <= N; i++);
     int j = 3;
 
     Console.WriteLine($"{i}^{j}={i^j}");
-    return 
+    return tablicaKubov
 }
 Console.WriteLine();
 
 Console.WriteLine("Введите ваше число:  ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int tablicaKubov(num)
-*/
+int tablicaKubov(num);
+
 
